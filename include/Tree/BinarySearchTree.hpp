e1f4bffa2e519bbd6bdddd38273e288bd82b8e73
totoo7 <stoyanorozov@gmail.com>
2024-12-09 15:31:52 +0200

    Added basic BinarySearchTree implementation

--- include/Tree/BinarySearchTree.hpp
+++ include/Tree/BinarySearchTree.hpp
@@ -1,8 +1,21 @@
 #ifndef BINARY_SEARCH_TREE_HPP
 #define BINARY_SEARCH_TREE_HPP
 
+#include<iostream>
+using namespace std;
+
 template<typename T>
 class BinarySearchTree {
+    public:
+        BinarySearchTree()=default;;
+        BinarySearchTree(const BinarySearchTree<T>& rhs);
+        BinarySearchTree<T>& operator=(const BinarySearchTree<T>& rhs);
+        bool find(const T& value);
+        void insert(const T& value);
+        bool remove(const T& value);
+        size_t height() const;
+        void print() const;   
+        ~BinarySearchTree();
     private:
         struct Node {
             T value;
@@ -11,23 +24,144 @@ class BinarySearchTree {
         };
         Node* root = nullptr;
     private:
-        Node*& find(Node* n, const T& value) {
-            if (!n) return false;
-            if (n->value == value) return true;
-            if (value < n->value) return find(n->left, value);
-            else return find(n->right, value);
-        }
-    public:
-        bool find(const T& value) {
-            return find(root, value);
-        }
-        bool insert(const T& value) {
-            Node*& pos = find(root, value);
-            if (pos) return false;
-            pos = new Node;
-            pos->value = value;
-            return true;
-        }
+        void clear(Node*& root); 
+        bool find(Node* n, const T& value);
+        Node* copy(Node* root);
+        void insert(Node*& root, const T& value);
+        bool remove(Node*& root, const T& value);
+        Node* get_min(Node*& root);
+        size_t height(const Node* root) const;
+        void print(const Node* root) const;
 };
+
+template<typename T>
+inline BinarySearchTree<T>::BinarySearchTree(const BinarySearchTree<T>& rhs) {
+    try {
+        root = copy(rhs.root);
+    } catch(std::bad_alloc& e) {
+        clear(root);
+        throw;
+    }
+}
+
+template<typename T>
+inline BinarySearchTree<T>& BinarySearchTree<T>::operator=(const BinarySearchTree<T>& rhs) {
+    if (this == &rhs) return *this;
+    BinarySearchTree<T> temp = rhs;
+    std::swap(root, temp.root);
+    return *this;
+}
+
+template<typename T>
+inline void BinarySearchTree<T>::insert(Node*& root, const T& value) {
+    if (!root) {
+        root = new Node;
+        root->value = value;
+    } else {
+        if (value < root->value) return insert(root->left, value);
+        else return insert(root->right, value);
+    }
+}
+
+template<typename T>
+inline void BinarySearchTree<T>::insert(const T& value) {
+    return insert(root, value);
+}   
+
+template<typename T>
+inline void BinarySearchTree<T>::clear(Node*& root) {
+    if (!root) return;
+    clear(root->left);
+    clear(root->left);
+    delete root;
+    root = nullptr;
+}
+
+template<typename T>
+inline bool BinarySearchTree<T>::find(const T& value) {
+    return find(root, value);
+}
+
+template<typename T>
+inline bool BinarySearchTree<T>::find(Node* n, const T& value) {
+    if (!n) return false;
+    if (n->value == value) return true;
+    if (value < n->value) return find(n->left, value);
+    else return find(n->right, value);
+}
+
+template<typename T>
+inline typename BinarySearchTree<T>::Node* BinarySearchTree<T>::copy(Node* root) {
+    if (!root) return root;
+    Node* n = new Node;
+    n->value = root->value;
+    n->left = copy(root->left);
+    n->right = copy(root->right);
+    return n;
+}
+
+template<typename T>
+inline bool BinarySearchTree<T>::remove(const T& value) {
+    return remove(root, value);
+}
+
+template<typename T>
+inline bool BinarySearchTree<T>::remove(Node*& root, const T& value) {
+    if (!root) return false;
+    if (root->value == value) {
+        Node* to_remove = root;
+        if (!root->left) root = root->right;
+        else if (!root->right) root = root->left;
+        else {
+            Node* min = get_min(root->right);
+            min->left = root->left;
+            min->right = root->right;
+
+        }
+        delete to_remove;
+        return true;
+    } else {
+        if (value < root->value) return remove(root->left, value);
+        else return remove(root->right, value);
+    }
+}
+
+template<typename T>
+inline typename BinarySearchTree<T>::Node* BinarySearchTree<T>::get_min(Node*& root) {
+    if (!root) return nullptr;
+    if (root->left) return get_min(root->left);
+    Node* n = root;
+    root = root->right;
+    return n;
+}
+
+template<typename T>
+inline size_t BinarySearchTree<T>::height(const Node* root) const {
+    if (!root) return 0;
+    return 1 + max(height(root->left), height(root->right));
+}
+
+template<typename T>
+inline size_t BinarySearchTree<T>::height() const {
+    return height(root);
+}
+
+template<typename T>
+inline void BinarySearchTree<T>::print(const Node* root) const {
+    if (!root) return;
+    print(root->left);
+    cout << root->value << " ";
+    print(root->right);
+}
+
+template<typename T>
+inline void BinarySearchTree<T>::print() const {
+    return print(root);
+}
+
+template<typename T>
+inline BinarySearchTree<T>::~BinarySearchTree() {
+    clear(root);
+}
 
 #endif
--- test/test_binary_search_tree.cpp
+++ test/test_binary_search_tree.cpp
@@ -3,5 +3,117 @@
 #include "Tree/BinarySearchTree.hpp"
 
 TEST_CASE("BinarySearchTree") {
-    BinarySearchTree<int> tree;
+    SECTION("Default Constructor and Empty Tree") {
+        BinarySearchTree<int> bst;
+        REQUIRE_FALSE(bst.find(10));
+    }
+
+    SECTION("Insert and Find") {
+        BinarySearchTree<int> bst;
+        bst.insert(10);
+        REQUIRE(bst.find(10));
+        bst.insert(10);
+        REQUIRE_FALSE(bst.find(20));
+    }
+
+    SECTION("Remove") {
+        BinarySearchTree<int> bst;
+        REQUIRE_FALSE(bst.remove(10));
+
+        bst.insert(10);
+        bst.insert(5);
+        bst.insert(20);
+
+        REQUIRE(bst.remove(5));
+        REQUIRE_FALSE(bst.find(5));
+
+        REQUIRE(bst.remove(10));
+        REQUIRE_FALSE(bst.find(10));
+
+        REQUIRE(bst.remove(20));
+        REQUIRE_FALSE(bst.find(20));
+    }
+
+    SECTION("Tree Height") {
+        BinarySearchTree<int> bst;
+        REQUIRE(bst.height() == 0);
+
+        bst.insert(10);
+        REQUIRE(bst.height() == 1);
+
+        bst.insert(5);
+        bst.insert(20);
+        REQUIRE(bst.height() == 2);
+
+        bst.insert(3);
+        REQUIRE(bst.height() == 3);
+    }
+
+    SECTION("Print Tree (In-Order Traversal)") {
+        BinarySearchTree<int> bst;
+        std::ostringstream oss;
+        auto original_cout = std::cout.rdbuf(oss.rdbuf());
+
+        bst.insert(10);
+        bst.insert(5);
+        bst.insert(15);
+
+        bst.print();
+        std::cout.rdbuf(original_cout);
+
+        REQUIRE(oss.str() == "5 10 15 ");
+    }
+
+    SECTION("Copy constructor") {
+        BinarySearchTree<int> tree1;
+        tree1.insert(10);
+        tree1.insert(5);
+        tree1.insert(15);
+        tree1.insert(3);
+        tree1.insert(7);
+        tree1.insert(12);
+        tree1.insert(17);
+
+        BinarySearchTree<int> tree2(tree1);
+
+        REQUIRE(tree2.find(10) == true);
+        REQUIRE(tree2.find(5) == true);
+        REQUIRE(tree2.find(15) == true);
+        REQUIRE(tree2.find(3) == true);
+        REQUIRE(tree2.find(7) == true);
+        REQUIRE(tree2.find(12) == true);
+        REQUIRE(tree2.find(17) == true);
+
+        tree2.insert(20);
+        REQUIRE(tree2.find(20) == true);
+        REQUIRE(tree1.find(20) == false);
+
+        tree2.remove(15);
+        REQUIRE(tree2.find(15) == false);
+        REQUIRE(tree1.find(15) == true);
+    }
+
+    SECTION("Assignment Operator") {
+        BinarySearchTree<int> bst1;
+        bst1.insert(10);
+        bst1.insert(5);
+
+        BinarySearchTree<int> bst2;
+        bst2.insert(20);
+
+        bst2 = bst1;
+
+        REQUIRE(bst2.find(10));
+        REQUIRE(bst2.find(5));
+        REQUIRE_FALSE(bst2.find(20));
+
+        bst1.remove(10);
+        REQUIRE(bst2.find(10));
+    }
+
+    SECTION("Destructor") {
+        BinarySearchTree<int>* bst = new BinarySearchTree<int>();
+        bst->insert(10);
+        delete bst;
+    }
 }
